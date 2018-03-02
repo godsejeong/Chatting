4842c1a702c78227145c0918ca0ddf473298d6b5
gotsejeong <dbdnpxm@naver.com>
2018-03-02 13:58:32 +0900

    sign up

---
+++ app/src/main/java/com/example/pc/chatting/RetrofitServer.kt
@@ -0,0 +1,13 @@
+package com.example.pc.chatting
+
+/**
+ * Created by pc on 2018-02-13.
+ */
+interface RetrofitServer{
+    var URL: String
+        get() = "iwin247.info"
+        set(value) = TODO()
+
+
+}
+
