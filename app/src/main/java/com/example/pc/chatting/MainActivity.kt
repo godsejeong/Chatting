d9d0aeb71d9638e0691e54ed6fd0f22c8b437faf
gotsejeong <dbdnpxm@naver.com>
2018-03-08 17:53:50 +0900

    layout_master

--- app/src/main/java/com/example/pc/chatting/MainActivity.kt
+++ app/src/main/java/com/example/pc/chatting/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.style.UnderlineSpan
 import android.text.SpannableString
+import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,5 +18,6 @@ class MainActivity : AppCompatActivity() {
         val content = SpannableString("Sing Up")
         content.setSpan(UnderlineSpan(), 0, content.length, 0)
         singUp.text = content
+        Log.e("asdf","asdf")
     }
 }
